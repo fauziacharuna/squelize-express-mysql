63c2771922b069b3110f27672e498a2fb1c9d4fa
fauzi <fauziharuna@gmail.com>
2021-03-15 08:37:31 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./app/models")
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:4000"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to  application." });
+});
+
+require("./app/routes/tutorial")(app)
+// set port, listen for requests
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
